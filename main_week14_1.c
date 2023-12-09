bf4278c26c85a37e0ece15ee3f461e34a38191c0
cyj4972 <144991384+cyj4972@users.noreply.github.com>
2023-12-09 15:42:21 +0900

    week14_2

--- main_week14_1.c
+++ main_week14_1.c
@@ -10,29 +10,23 @@ struct Book
 };
 
 int main(int argc, char *argv[])
-{	int i;
-	struct Book **bookshelf;
-	
-	bookshelf = (struct Book**)malloc(sizeof(struct Book*));		//구조체 포인터 변수 3개 할당 
-	for(i=0;i<3;i++)
-		bookshelf[i] = malloc(10*sizeof(struct Book));		//구조체 10개 할당 
-	
-	bookshelf[1][3].number = 5;
-	strcpy(bookshelf[1][3].title, "C++ Programming");
-	
-	(bookshelf[2]+4)->number = 3;
-	strcpy((bookshelf[2]+4)->title, "Communications Theory");
-	
-	printf("book(1,3): %i, %s\n",(bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
-	printf("book(2,4): %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
-	
-	//구조체 포인터 3개가 가각 가리키는 주소 해제
-	for (i=0;i<3;i++)
-		free(bookshelf[i]);
-		
-	//구조체 이중 포인터가 가리키는 주소 해제
-	free(bookshelf);
-		
+{	struct Book *p;
+
+   p = (struct Book*)malloc(2*sizeof(struct Book));
+   
+   if(p == NULL)
+   {
+      printf("메모리 할당 오류\n");
+      return;
+   }
+   
+   p->number = 1;
+   strcpy(p->title, "C Programming");
+   
+   (p+1)->number = 2;
+   strcpy((p+1)->title, "Electronics");
+   
+   free(p);	
 	system("PAUSE");
 	return 0;
 }
